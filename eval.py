b838c0d34a6b5a00b585f39feb5f67f68e8a6e76
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2019-10-24 14:46:24 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -5,18 +5,7 @@ from hparams import hparams, hparams_debug_string
 from synthesizer import Synthesizer
 
 
-sentences = [
-  # From July 8, 2017 New York Times:
-  'Scientists at the CERN laboratory say they have discovered a new particle.',
-  'There’s a way to measure the acute emotional intelligence that has never gone out of style.',
-  'President Trump met with other leaders at the Group of 20 conference.',
-  'The Senate\'s bill to repeal and replace the Affordable Care Act is now imperiled.',
-  # From Google's Tacotron example page:
-  'Generative adversarial network or variational auto-encoder.',
-  'The buses aren\'t the problem, they actually provide a solution.',
-  'Does the quick brown fox jump over the lazy dog?',
-  'Talib Kweli confirmed to AllHipHop that he will be releasing an album in the next year.',
-]
+
 
 
 def get_output_base_path(checkpoint_path):
@@ -30,12 +19,20 @@ def run_eval(args):
   print(hparams_debug_string())
   synth = Synthesizer()
   synth.load(args.checkpoint)
-  base_path = get_output_base_path(args.checkpoint)
-  for i, text in enumerate(sentences):
-    path = '%s-%d.wav' % (base_path, i)
-    print('Synthesizing: %s' % path)
-    with open(path, 'wb') as f:
-      f.write(synth.synthesize(text))
+  for fi in os.listdir(args.ppgs_dir):
+    ppgs_path = os.path.join(ppgs_dir, fi)
+    ppgs = np.load(ppgs_path)
+    speaker_list = {'biaobei':1, ts':2}
+    spk = 'biaobei'
+    spk_id  =speaker_list[spk]
+    lpc = synth.synthesize(spk_id, ppgs)
+    out_path = os.path.join(args.out_dir, 'lpc32)
+    os.makedisr(out_path, exist_ok=True)
+    path = os.path.join(out_path, '%s_%s.npy'%(ppgs_name, spk))
+    np.save(path, lpc)
+    print('saved', path)
+  
+  
 
 
 def main():
@@ -43,8 +40,10 @@ def main():
   parser.add_argument('--checkpoint', required=True, help='Path to model checkpoint')
   parser.add_argument('--hparams', default='',
     help='Hyperparameter overrides as a comma-separated list of name=value pairs')
+  parser.add_argument('--ppgs_dir', help='ppgs_dir')
+  parser.add_argument('--out_dir', help='save_dir')
   args = parser.parse_args()
-  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
   hparams.parse(args.hparams)
   run_eval(args)
 
