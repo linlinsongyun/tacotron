a771894c544f8c9a65c85a088027b736c7120fbe
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2019-10-24 14:52:22 +0800

    Update synthesizer.py

--- synthesizer.py
+++ synthesizer.py
@@ -4,19 +4,20 @@ import tensorflow as tf
 from hparams import hparams
 from librosa import effects
 from models import create_model
-from text import text_to_sequence
 from util import audio
 
 
 class Synthesizer:
   def load(self, checkpoint_path, model_name='tacotron'):
     print('Constructing model: %s' % model_name)
-    inputs = tf.placeholder(tf.int32, [1, None], 'inputs')
+    speaker_id = tf.placeholder(tf.int32, [None], 'speaker_id')
+    mask = tf.placeholder(tf.float32, [None, None, 32], 'mask')
+    inputs = tf.placeholder(tf.float32, [None, None, 249], 'inputs')
     input_lengths = tf.placeholder(tf.int32, [1], 'input_lengths')
     with tf.variable_scope('model') as scope:
       self.model = create_model(model_name, hparams)
-      self.model.initialize(inputs, input_lengths)
-      self.wav_output = audio.inv_spectrogram_tensorflow(self.model.linear_outputs[0])
+      self.model.initialize(speaker_id, mask, inputs, input_lengths)
+      self.mel_outputs = self.model.outputs[0]
 
     print('Loading checkpoint: %s' % checkpoint_path)
     self.session = tf.Session()
@@ -25,16 +26,9 @@ class Synthesizer:
     saver.restore(self.session, checkpoint_path)
 
 
-  def synthesize(self, text):
-    cleaner_names = [x.strip() for x in hparams.cleaners.split(',')]
-    seq = text_to_sequence(text, cleaner_names)
-    feed_dict = {
-      self.model.inputs: [np.asarray(seq, dtype=np.int32)],
-      self.model.input_lengths: np.asarray([len(seq)], dtype=np.int32)
-    }
-    wav = self.session.run(self.wav_output, feed_dict=feed_dict)
-    wav = audio.inv_preemphasis(wav)
-    wav = wav[:audio.find_endpoint(wav)]
-    out = io.BytesIO()
-    audio.save_wav(wav, out)
-    return out.getvalue()
+  def synthesize(self, speaker_id, ppgs):
+    feed_dict = {self.model.speaker_id: np.asarray([speaker_id], dtype=np.int32),
+                 self.model.inputs: [np.asarray(ppgs, dtype=np.float32)],
+                 self.model.input_lengths: np.asarray([len(ppgs)], dtype=np.int32)}
+    lpc = self.session.run(self.mel_outputs, feed_dic=feed_dic)
+    return lpc
